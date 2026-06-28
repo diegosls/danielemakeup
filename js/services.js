2576aa67c8d215dc8497876820ea5ef6301f2363
diegosls <162706801+diegosls@users.noreply.github.com>
2026-06-28 16:14:29 -0300

    Update API endpoint to production URL

--- js/services.js
+++ js/services.js
@@ -1,4 +1,4 @@
-const API = "http://127.0.0.1:5000/api/services";
+const API = "https://danielemakeup.onrender.com/api/services";
 
 document.addEventListener("DOMContentLoaded", carregarServicos);
 
